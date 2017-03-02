84d0537c9bc6e5aa2650277150224d61ec0f33cd
ashudvlps <ashudvlps@gmail.com>
2017-03-02 00:29:49 -0800

    pushing v0.1

---
+++ lists.py
@@ -0,0 +1,24 @@
+# Solution to the problem at
+# https://www.hackerrank.com/challenges/python-lists
+N = int(input())
+flist = []
+templist = []
+for i in range(0,N):
+    inp = input()
+    inpl = inp.split(' ')
+    if inpl[0]=='insert':
+        templist.insert(int(inpl[1]),int(inpl[2]))
+    elif inpl[0]=='print':
+        flist.append(tuple(templist))
+    elif inpl[0]=='remove':
+        templist.remove(int(inpl[1]))
+    elif inpl[0]=='append':
+        templist.append(int(inpl[1]))
+    elif inpl[0]=='sort':
+        templist.sort()
+    elif inpl[0]=='pop':
+        templist.pop()
+    elif inpl[0]=='reverse':
+        templist.reverse()
+for rows in flist:
+    print(list(rows))
