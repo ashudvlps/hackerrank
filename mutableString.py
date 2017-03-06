989a6ed838c552c8b46a891ccc0aab4639b8efa6
ashudvlps <ashudvlps@gmail.com>
2017-03-05 22:06:37 -0800

    V0.01c

--- mutableString.py
+++ mutableString.py
@@ -1,8 +1,6 @@
 # You are given an immutable string, and you want to make changes to it.
-val = input()
+# val = input()
+values = list(input())
 mutVal = input().split(" ")
-values = []
-for v in val:
-    values.append(v)
 values[int(mutVal[0])] = mutVal[1]
 print("".join(values))
