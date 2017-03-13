762cc66f19eb98ea7d39725300a2a616c1107b64
ashudvlps <ashudvlps@gmail.com>
2017-03-12 21:45:12 -0700

    V0.01

--- doorMat.py
+++ doorMat.py
@@ -1,4 +1,6 @@
-N, M = map(int,input().split())
+N, M = map(int,input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
 for i in range(1,N,2):
-    middlePrint = ".|."
-    print(middlePrint*1)
+    print('{spr:-^{width}s}'.format(width=M, spr='.|.'*i))
+print ('{spr:-^{width}s}'.format(width=M, spr='WELCOME'))
+for i in range(N-2,-1,-2):
+    print('{spr:-^{width}s}'.format(width=M, spr='.|.'*i))
